2d01e4b97276028c57edd0503fe1339941cd61c4
Chavozoom <isaquechaves10500@gmail.com>
2023-09-26 18:52:27 -0300

    creating operation of instantion

--- src/main/java/com/chavozo/webservice/WebserviceApplication.java
+++ src/main/java/com/chavozo/webservice/WebserviceApplication.java
@@ -1,10 +1,28 @@
 package com.chavozo.webservice;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.chavozo.webservice.domain.Category;
+import com.chavozo.webservice.repositories.CategoryRepository;
+
 @SpringBootApplication
-public class WebserviceApplication {
+public class WebserviceApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoryRepository categoryRepository;
+
+	@Override
+	public void run(String[] args) throws Exception {
+		Category cat1 = new Category(null, "Office");
+		Category cat2 = new Category(null, "House");
+
+		categoryRepository.saveAll(Arrays.asList(cat1, cat2));
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(WebserviceApplication.class, args);
