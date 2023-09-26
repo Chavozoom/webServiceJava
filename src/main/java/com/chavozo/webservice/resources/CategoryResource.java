70df08e00e9dc1a954cd420a1f8842b8353b896d
Chavozoom <isaquechaves10500@gmail.com>
2023-09-26 18:48:07 -0300

    creating service and repository to category

--- src/main/java/com/chavozo/webservice/resources/CategoryResource.java
+++ src/main/java/com/chavozo/webservice/resources/CategoryResource.java
@@ -1,26 +1,26 @@
 package com.chavozo.webservice.resources;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import com.chavozo.webservice.domain.Category;
+import com.chavozo.webservice.service.CategoryService;
 
 @RestController
 @RequestMapping(value = "/categories")
 public class CategoryResource {
 
-    @RequestMapping(method = RequestMethod.GET)
-    public List<Category> list() {
-        Category cat1 = new Category(1, "Info");
-        Category cat2 = new Category(2, "Office");
+    @Autowired
+    private CategoryService service;
 
-        List<Category> list = new ArrayList<>();
-        list.add(cat1);
-        list.add(cat2);
-        return list;
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public ResponseEntity<?> find(@PathVariable Integer id) {
+
+        Category category = service.find(id);
+        return ResponseEntity.ok().body(category);
     }
 }
---
+++ src/main/java/com/chavozo/webservice/service/CategoryService.java
@@ -0,0 +1,22 @@
+package com.chavozo.webservice.service;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.chavozo.webservice.domain.Category;
+import com.chavozo.webservice.repositories.CategoryRepository;
+
+@Service
+public class CategoryService {
+
+    @Autowired
+    private CategoryRepository repository;
+
+    public Category find(Integer id) {
+        Optional<Category> category = repository.findById(id);
+        return category.orElse(null);
+    }
+
+}
