01618d6ea209a65cdcacc111ea7ffa1626a3f5e0
Chavozoom <isaquechaves10500@gmail.com>
2023-09-26 16:09:05 -0300

    first commit

---
+++ src/main/java/com/chavozo/webservice/resources/CategoryResource.java
@@ -0,0 +1,26 @@
+package com.chavozo.webservice.resources;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.chavozo.webservice.domain.Category;
+
+@RestController
+@RequestMapping(value = "/categories")
+public class CategoryResource {
+
+    @RequestMapping(method = RequestMethod.GET)
+    public List<Category> list() {
+        Category cat1 = new Category(1, "Info");
+        Category cat2 = new Category(2, "Office");
+
+        List<Category> list = new ArrayList<>();
+        list.add(cat1);
+        list.add(cat2);
+        return list;
+    }
+}
